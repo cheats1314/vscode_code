9e6f595a9b23766e52fbc671c8ea9bfb96239a33
cheats1314 <3030240693@qq.com>
2025-03-01 22:32:50 +0800

    1232

--- code/t1.cpp
+++ code/t1.cpp
@@ -11,61 +11,35 @@ using ll = long long;
 const int N = 1e5 + 10;
 int h[N], e[2 * N], ne[N];
 const int MOD = 1e9 + 7;
-int mul(int x, int y) {return 1LL * x * y % MOD; }
-int fact[N], ifact[N];
-
-int fastpow(int x, int y) {
-    int res = 1;
-    while(y) {
-        if (y &  1) res = mul(res, x);
-        x = mul(x, x), y >>= 1;
-    }
-    return res;
-}
-
-int C(int n, int m) {
-    if (m == 0 || m == n) return 1;
-    return mul(fact[n], mul(ifact[m], ifact[n-m]));
-}
 
 
 void solve() {
-    int n;
-    string s;
-    cin >> n >> s;
-    fact[0] = 1;
-    for (int i = 1; i <= n; i++) fact[i] = mul(i, fact[i-1]);
-    ifact[n] = fastpow(fact[n], MOD - 2);
-    for (int i = n; i >= 1; i--) ifact[i-1] = mul(i, ifact[i]);
-    
-    vector<int> win(n + 1), fail(n + 1);
-    for (int i = 0; s[i]; i++) {
-        if (s[i] == '0') fail[i+1]=fail[i]+1, win[i+1]=win[i];
-        else win[i+1]=win[i]+1, fail[i+1]=fail[i];
-    }
-    bool status = true;
-    for (int i = 1; i <= n; i++) {
-        if (fail[i] > win[i]) {
-            status = false;
-            break;
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > a(n);
+        for (int i = 0; i < n; i++) {
+            a[i].resize(n);
+            for (int j = 0; j < n; j++) cin >> a[i][j];
+            sort(begin(a[i]), end(a[i]));
         }
-    }
-    if (status) cout << n;
-    else {
-        for (int i = 1; i <= n; i++) {
-            if (fail[i] - win[i] > 2) {
-                cout << 0;
-                return;
+        vector<vector<int> > b(n);
+        b[0].resize(1);
+        b[0][0] = a[0][0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] >= a[i-1][0] && b[i].size() < i + 1) {
+                    b[i].push_back(a[i][j]);
+                }
             }
         }
-        for (int i = 1; i <= n; i++) {
-            if (fail[i] > win[i]) {
-                cout << fail[i];
-                break;
-            }
-        }
+        // 验证
+        for (int i = 0; i < n; i++) OUT(b[i]);
     }
 }
+
 
 signed main() {
     fast;
