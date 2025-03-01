5e3c50b2e00999b21249052ac3e2bb883642005f
cheats1314 <3030240693@qq.com>
2025-03-01 22:38:16 +0800

    1234

--- code/python/1.py
+++
@@ -1,7 +0,0 @@
-def print_pyc_header(pyc_file):
-    with open('123.pyc', "rb") as f:
-        header = f.read(16)  # 读取前 16 个字节
-        print(f"Header: {header.hex()}")
-
-# 使用示例
-print_pyc_header("到梦空间-捡漏.pyc")
